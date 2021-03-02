f629804f66c62cac1cf7fbf326639595d3bd4ea8
Ahire Hitesh <hbahire2014@gmail.com>
2021-03-02 16:33:51 +0530

    update

--- link list/doubly linked list/doubly linked.cpp
+++ link list/doubly linked list/doubly linked.cpp
@@ -15,77 +15,74 @@ NODEPTR createLi(NODEPTR dlist){
 	NODEPTR newnode,temp;
 	temp = dlist;
 	dlist = NULL;
-	printf("\t How many nodes :-");
+	printf("\n\t How many nodes :-\t");
 	scanf("%d",&n);
-	
-	printf("\t Enter the newnode:-");
+
 	for(count = 1; count <= n; count++){
-		
+        printf("\n\t Enter Node :\t");
+
 		newnode = (struct node*) malloc(sizeof(struct node));
-		
+
 		scanf("%d",&newnode -> data);
+
 		newnode ->next = NULL;
 		newnode->prev = NULL;
-	if(dlist == NULL){
-	
-		dlist = newnode;
-		temp = newnode;
-	}
-	else{
-	
-		temp -> next = newnode;
-		temp = newnode;
+        if(dlist == NULL){
+            dlist = newnode;
+            temp = newnode;
+        }else{
+            temp -> next = newnode;
+            temp = newnode;
 	    }
-    } 
+    }
 	return(dlist);
 }
 
-
  NODEPTR insBeg(NODEPTR dlist){
 	NODEPTR temp,newnode;
-	printf("\tEnter the newnode");
+	printf("\n\t Enter the newnode :\t");
+
 	newnode = (struct node*) malloc(sizeof(struct node));
 
 	scanf("%d",&newnode->data);
-	    newnode->next = dlist;
-		dlist->prev = newnode;
-		newnode->prev = NULL;
-		dlist = newnode;
+
+    newnode->next = dlist;
+    dlist->prev = newnode;
+    newnode->prev = NULL;
+    dlist = newnode;
+
 	return(dlist);
 }
 
 
 NODEPTR insEnd(NODEPTR dlist){
 	NODEPTR temp,newnode;
-	
+
 	temp = dlist;
 	while(temp->next != NULL)
 		temp = temp->next;
-	
-	printf("\t Enter the newnode");
+
+	printf("\t Enter the newnode :\t");
 	newnode = (struct node*) malloc(sizeof(struct node));
-	 scanf("%d",&newnode->data);
-	    newnode->next = NULL;
-		newnode->prev = temp;
-		temp->next = newnode;
-		dlist = newnode;
-	
+    scanf("%d",&newnode->data);
+    newnode->next = NULL;
+    newnode->prev = temp;
+    temp->next = newnode;
+    //dlist = newnode;
+
 	return(dlist);
 }
 
 void disList(NODEPTR dlist){
 	NODEPTR temp,newnode;
-	
+
 	temp = dlist;
+	printf("\n\t list is :  ");
+
 	while(temp!= NULL){
-			
-		printf("\t ordered list is %d\n",temp->data);
+
+		printf(" %d",temp->data);
 		temp = temp->next;
-	}
-	while(temp!= NULL){
-		
-	    printf("The reverse list is %d\n",temp->data);
-	    temp = temp-> prev; 
 	}
 }
 
@@ -95,26 +92,26 @@ NODEPTR insBet(NODEPTR dlist){
 	int count = 1,pos;
 	NODEPTR temp,newnode;
 	temp = dlist;
-	
+
 	printf("Enter the position");
 	scanf("%d",&pos);
-	
+
 	while(count < pos-1){
 		temp = temp->next;
 		count++;
 	}
 		printf("Enter newnode");
-		
+
 		newnode = (struct node*) malloc(sizeof(struct node));
 		scanf("%d",&newnode->data);
-	   
+
 		newnode-> prev = temp;
 		newnode->next = temp->next;
 		temp ->next = newnode;
 		(newnode->next)-> prev = newnode;
-	
+
 	return(dlist);
-	
+
 }
 NODEPTR delBeg(NODEPTR dlist){
 		NODEPTR temp;
@@ -130,75 +127,72 @@ NODEPTR delEnd(NODEPTR dlist){
 	    temp = dlist;
 		while(temp->next != NULL)
 			temp = temp->next;
-			
-		(temp->prev)->next = NULL;		
+
+		(temp->prev)->next = NULL;
 		free(temp);
 	return(dlist);
 }
 
 NODEPTR delBet(NODEPTR dlist){
-	 NODEPTR  temp,temp1,newnode;
-	 int count = 1,pos;
-	 
-	 printf("\t Enter the pos:-");
-	 scanf("%d",&pos);
-	 temp = dlist;
-	 while( count <= pos-1){
-	 	
-	 	count++;
-	 }
-	 temp->next = temp1 ->next;
-	 free(temp1);
+    NODEPTR  temp,temp1;
+    int count ,pos;
+    printf("\n\t Enter Position :\t");
+    scanf("%d", &pos);
+    for(count = 1; count < pos-1; count++)
+
 	 return(dlist);
 }
 
 
 int main(){
+
 	NODEPTR dlist,newnode;
+
 	int choice,n,pos;
 	dlist = createLi(dlist);
 	disList(dlist);
 
 	 do{
-	 	printf("***operation of Insertion ***");
-	 	printf("\n 1  Insert begin");
-	 	printf("\n 2  Insert End");
-	 	printf("\n 3  Insert Between");
-	 	printf("\n 4  Delete begin");
-	 	printf("\n 5  Delete End");
-	 	printf("\n 6  Delete  Between");
-	 	
-	 	printf("\n Enter your choice:-");
+	 	printf("\n\t ***operation of Insertion ***");
+	 	printf("\n\t 1  Insert begin");
+	 	printf("\n\t 2  Insert End");
+	 	printf("\n\t 3  Insert Between");
+	 	printf("\n\t 4  Delete begin");
+	 	printf("\n\t 5  Delete End");
+	 	printf("\n\t 6  Delete  Between");
+
+	 	printf("\n\t Enter your choice :-\t");
 	 	scanf("%d",&choice);
+
 	 	switch(choice){
 	 		case 1: dlist = insBeg(dlist);
 			        disList(dlist);
 	 		break;
-	 				
+
 	 	    case 2: insEnd(dlist);
 			 		disList(dlist);
 	 		break;
-	 			
+
 	 		case 3: insBet(dlist);
 			        disList(dlist);
 	 		break;
-	 			
+
 	 		case 4: dlist = delBeg(dlist);
 	 	    	    disList(dlist);
 	 		break;
-	 			
+
 	 		case 5: dlist = delEnd(dlist);
 			 		disList(dlist);
 	 		break;
-	 		
+
 	 		case 6: delBet(dlist);
 			 		disList(dlist);
 	 		break;
-		
+
 		 }
-	 	
-	 	
+
+
 	 }while(choice != 7);
-	 
+
 	return(0);
 }
